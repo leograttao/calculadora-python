1da610d7ff86b095c33971e74c6ebe205a3a604c
leograttao <162933108+leograttao@users.noreply.github.com>
2024-03-13 15:16:31 -0300

    Add files via upload

---
+++ Calculadora-Puc/calculadora.py
@@ -0,0 +1,17 @@
+numero1 = int(input("Digite o primeiro número para operaçao: "))
+numero2 = int(input("Digite o segundo número para operação: "))
+
+print("Selecione o número para a operação desejada:\n\n1 - Soma\n2 - Subtração\n3 - multiplicaçao\n4 - Divisão\n5 - Sair")
+
+opcao_de_operacao = int(input("Digite sua opção(1/2/3/4/5): "))
+
+if opcao_de_operacao == 5:
+    print("Você saiu da calculadora")
+elif opcao_de_operacao == 1:
+    print(f"{numero1} + {numero2} =", numero1 + numero2)
+elif opcao_de_operacao == 2:
+    print(f"{numero1} + {numero2} =", numero1 - numero2)
+elif opcao_de_operacao ==  3:
+    print(f"{numero1} x {numero2} =", numero1 * numero2)
+elif opcao_de_operacao == 4:
+    print(f"{numero1} / {numero2} =", numero1 / numero2)
