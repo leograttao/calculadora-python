b632df9052d52ed5ed7550befa8376067276ca7d
leograttao <162933108+leograttao@users.noreply.github.com>
2024-03-27 12:20:37 -0300

    Add files via upload

--- aula02_tarefa.py
+++ aula02_tarefa.py
@@ -1,14 +1,17 @@
 opcao_de_operacao = 0
 
 while opcao_de_operacao != 5:
-    print("Selecione o número para a operação desejada:\n\n1 - Soma\n2 - Subtração\n3 - multiplicaçao\n4 - Divisão\n5 - Sair")
+    print("\nSelecione o número para a operação desejada:\n\n1 - Soma\n2 - Subtração\n3 - multiplicaçao\n4 - Divisão\n5 - Sair")
     
     opcao_de_operacao = int(input("Digite sua opção(1/2/3/4/5): "))
     
-    if opcao_de_operacao == 5:
+    if opcao_de_operacao > 5 or opcao_de_operacao < 0:
+        print("\nErro, selecione entre 1 e 5\n\n")
+        continue  
+    elif opcao_de_operacao == 5:
         print("Você saiu da calculadora")
         break
-    
+
     numero1 = int(input("Digite o primeiro número para operaçao: "))
     numero2 = int(input("Digite o segundo número para operação: "))
     
@@ -20,6 +23,3 @@ while opcao_de_operacao != 5:
         print(f"{numero1} x {numero2} =", numero1 * numero2)
     elif opcao_de_operacao == 4:
         print(f"{numero1} / {numero2} =", numero1 / numero2)
-
-    if opcao_de_operacao > 5 or opcao_de_operacao < 0:
-        print("Selecione entre 1 e 5\n\n")
